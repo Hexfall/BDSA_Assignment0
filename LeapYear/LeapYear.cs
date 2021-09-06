4485e8a83b5d143eeb4d52c1dfdfccfa991b87be
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-09-06 13:10:11 +0200

    Tests for LeapYear.

---
+++ LeapYear/LeapYear.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LeapYear
+{
+    public class LeapYear
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public static bool IsLeapYear(int year)
+        {
+            return false;
+        }
+    }
+}
