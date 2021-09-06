d8440c55389c443df4e56b41888c87be719642db
Viktor Máni Jensson <Viktor.mani@hotmail.co.uk>
2021-09-06 13:24:52 +0200

    Implemented IsLeapYear.

--- LeapYear/LeapYear.cs
+++ LeapYear/LeapYear.cs
@@ -10,6 +10,12 @@ namespace LeapYear
 
         public static bool IsLeapYear(int year)
         {
+            if (year % 400 == 0)
+                return true;
+            if (year % 100 == 0)
+                return false;
+            if (year % 4 == 0)
+                return true;
             return false;
         }
     }
