805dd70f9533db4bc3eb39608da8748869933ad2
Hexfall <42968012+Hexfall@users.noreply.github.com>
2021-09-09 20:13:40 +0200

    Errorhandling/user input (#3)
    * General program error handling

--- LeapYear.Tests/LeapYearTests.cs
+++ LeapYear.Tests/LeapYearTests.cs
@@ -55,5 +55,99 @@ namespace LeapYear.Tests
             string actualOutput = writer.GetStringBuilder().ToString();
             Assert.Equal(expectedOutput, actualOutput);
         }
+
+        [Fact]
+        public void Throws_Error_Before_1582()
+        {
+            List<int> years = new()
+            {
+                -15,
+                1,
+                56,
+                1444,
+                1580,
+                1581
+            };
+            foreach (int y in years)
+            {
+                try
+                {
+                    LeapYear.IsLeapYear(y);
+                    throw new InvalidOperationException("Earlier line should have thrown exception.");
+                }
+                catch (Exception e)
+                {
+                    Assert.Equal(new ArgumentOutOfRangeException().GetType(), e.GetType());
+                }
+            }
+        }
+
+        [Fact]
+        public void Console_Responds_Correctly_Below_1582()
+        {
+            string inputString = 1581.ToString() + Environment.NewLine + "q" + Environment.NewLine;
+            var reader = new StringReader(inputString);
+            var writer = new StringWriter();
+
+            Console.SetIn(reader);
+            Console.SetOut(writer);
+            LeapYear.InteractiveLeapCalander();
+            string actualOutput = writer.GetStringBuilder().ToString();
+
+
+            string expectedOutput = $"Input year to check (or \'q\' to exit):{Environment.NewLine}";
+            expectedOutput += $"1582 marks the introduction of the Gregorian calendar, so dates before then are invalid{Environment.NewLine}";
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void IsLeapYear_Throws_Error_For_0()
+        {
+            try
+            {
+                LeapYear.IsLeapYear(0);
+                throw new InvalidOperationException("Earlier line should have thrown exception.");
+            }
+            catch (Exception e)
+            {
+                Assert.Equal(new ArgumentNullException().GetType(), e.GetType());
+            }
+        }
+
+        [Fact]
+        public void Console_Responds_Correctly_To_0()
+        {
+            string inputString = 0.ToString() + Environment.NewLine + "q" + Environment.NewLine;
+            var reader = new StringReader(inputString);
+            var writer = new StringWriter();
+
+            Console.SetIn(reader);
+            Console.SetOut(writer);
+            LeapYear.InteractiveLeapCalander();
+            string actualOutput = writer.GetStringBuilder().ToString();
+
+
+            string expectedOutput = $"Input year to check (or \'q\' to exit):{Environment.NewLine}";
+            expectedOutput += $"The year 0 AD does not exist. The Gregorian calendar goes from 1 BC to 1 AD{Environment.NewLine}";
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
+        [Fact]
+        public void Catches_Non_Numbers_And_Responds_Accordingly()
+        {
+            string inputString = "Not a number" + Environment.NewLine + "q" + Environment.NewLine;
+            var reader = new StringReader(inputString);
+            var writer = new StringWriter();
+
+            Console.SetIn(reader);
+            Console.SetOut(writer);
+            LeapYear.InteractiveLeapCalander();
+            string actualOutput = writer.GetStringBuilder().ToString();
+
+
+            string expectedOutput = $"Input year to check (or \'q\' to exit):{Environment.NewLine}";
+            expectedOutput += $"Input must be a whole number{Environment.NewLine}";
+            Assert.Equal(expectedOutput, actualOutput);
+        }
     }
 }
--- LeapYear/LeapYear.cs
+++ LeapYear/LeapYear.cs
@@ -11,6 +11,10 @@ namespace LeapYear
 
         public static bool IsLeapYear(int year)
         {
+            if (year == 0)
+                throw new ArgumentNullException("0 AD does not exist");
+            if (year < 1582)
+                throw new ArgumentOutOfRangeException("Does not apply to years before 1582.");
             if (year % 400 == 0)
                 return true;
             if (year % 100 == 0)
@@ -26,9 +30,23 @@ namespace LeapYear
             string input = Console.ReadLine().Trim();
             while (!(input == "q"))
             {
-                var num = int.Parse(input);
-                Console.WriteLine(IsLeapYear(num) ? "yay" : "nay");
-
+                try
+                {
+                    var num = int.Parse(input);
+                    Console.WriteLine(IsLeapYear(num) ? "yay" : "nay");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("1582 marks the introduction of the Gregorian calendar, so dates before then are invalid");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("The year 0 AD does not exist. The Gregorian calendar goes from 1 BC to 1 AD");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Input must be a whole number");
+                }
                 input = Console.ReadLine().Trim();
             }
         }
